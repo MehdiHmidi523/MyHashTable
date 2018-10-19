373647b9e83ad87f85b232618c8e80c79867d273
Mehdi Hmidi <mh223vk@student.lnu.se>
2018-10-19 12:40:06 +0200

    final rush: EX3 skeleton code

---
+++ src/MyMeasure.java
@@ -0,0 +1,19 @@
+/**
+ * Created by Mehdi on 19/10/2018 for the MyHashTable project.
+ */
+public class MyMeasure implements A2Measure {
+    @Override
+    public boolean isSameCollection(int[] array1, int[] array2) {
+        return false;
+    }
+
+    @Override
+    public int minDifferences(int[] array1, int[] array2) {
+        return 0;
+    }
+
+    @Override
+    public int[] getPercentileRange(int[] arr, int lower, int upper) {
+        return new int[0];
+    }
+}
--- src/Point.java
+++ src/Point.java
@@ -3,44 +3,47 @@
  */
 public class Point {
 
-        public Point next;
-        private int x, y, pos;
+    public Point next;
+    private int x, y, pos;
 
-        public Point(int x1, int y1) {
-            this.x = x1;
-            this.y = y1;
-        }
+    public Point(int x1, int y1) {
+        this.x = x1;
+        this.y = y1;
+    }
 
-        int getX() {
-            return x;
-        }
+    int getX() {
+        return x;
+    }
 
-        int getY() {
-            return y;
-        }
+    int getY() {
+        return y;
+    }
 
-        void setX(int x) {
-            this.x = x;
-        }
+    void setX(int x) {
+        this.x = x;
+    }
 
-        void setY(int y) {
-            this.y = y;
-        }
+    void setY(int y) {
+        this.y = y;
+    }
 
-        int getPos() {
-            return pos;
-        }
+    int getPos() {
+        return pos;
+    }
 
-        void setPos(int pos) {
-            this.pos = pos;
-        }
+    void setPos(int pos) {
+        this.pos = pos;
+    }
 
-        @Override
-        public boolean equals(Object o) { return o instanceof Point &&((Point) o).x == this.x && ((Point) o).y == this.y; }
-        @Override
-        public int hashCode(){
-            int tmp = ( y +  ((x+1)/2));
-            return x +  ( tmp * tmp);//bijective algorithm
-        }
+    @Override
+    public boolean equals(Object o) {
+        return o instanceof Point && ((Point) o).x == this.x && ((Point) o).y == this.y;
+    }
+
+    @Override
+    public int hashCode() {
+        int tmp = (y + ((x + 1) / 2));
+        return x + (tmp * tmp);//bijective algorithm
+    }
 }
 
